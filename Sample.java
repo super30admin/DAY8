782eac3e07eec3af26a583799fb894f88d8f9a9b
Sai Ashok Kumar Reddy Paluru <reddysaiashok29@gmail.com>
2024-04-13 00:16:46 -0500

    Revert "DP-1 this is awesome. Thanks."
    This reverts commit aba1fb5d872a3f8c3b83058d0639a64daaa84cb7.

--- Sample.java
+++ Sample.java
@@ -3,13 +3,5 @@
 // Did this code successfully run on Leetcode :
 // Any problem you faced while coding this :
 
-/*
- * 
- */
-public class HousePaintingDP {
 
-    public int minCost(int[][] costs) {
-
-    }
-
-}
+// Your code here along with comments explaining your approach
