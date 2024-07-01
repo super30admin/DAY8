194deca3b1529dc528937d0561a62e23bfe4a326
jayanthVishwas <vishwasjayanth45@gmail.com>
2024-07-01 00:27:32 -0500

    completed dp2

---
+++ problem2.py
@@ -0,0 +1,27 @@
+class Solution:
+    # time: O(n2)
+    # space: O(n2)
+    def change(self, amount: int, coins: List[int]) -> int:
+        
+        dp = [[0 for i in range(amount+1)] for j in range(len(coins)+1)]
+        
+        for i in range(len(dp)):
+            dp[i][0] = 1
+            
+         # i = amount, j= coins    
+        for coin in range(1,len(dp)):
+            for amt in range(1, len(dp[0])):
+                if amt < coins[coin-1]:
+                    dp[coin][amt] = dp[coin-1][amt]
+                    
+                else:
+                    dp[coin][amt] = dp[coin-1][amt] + dp[coin][amt - coins[coin-1]]
+                    
+        return dp[-1][-1]
+            
+            
+        
+        
+        
+        
+        
