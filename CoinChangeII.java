65a706640e9feceb8896f6b8dbf5a9d065b6d04a
firoz Syed <syedfiroz2010@gmail.com>
2024-11-05 23:21:44 -0800

    DP With T size array

--- CoinChangeII.java
+++ CoinChangeII.java
@@ -27,3 +27,22 @@ class Solution {
         return result;
     }
 }
+
+// Approach : DP Array with just T space
+// Time : O(n*t)
+// Space: O(t)
+class Solution {
+    public int change(int amount, int[] coins) {
+        int[] dp = new int[amount+1];
+        dp[0]=1;
+        for(int c:coins){
+            for(int i=c;i<=amount;i++){
+                // take the row above
+                dp[i]+=dp[i-c];
+
+        };
+        }
+        return dp[amount]; // last element in single array
+        
+    }
+}
